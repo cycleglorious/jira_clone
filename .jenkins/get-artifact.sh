5cb4fe36b5fe60ddbdc79548f6ec6308da2c9be6
OleksiuDatsko <datskooleksiu@gmail.com>
2025-02-20 15:47:02 +0200

    fixed get-artifact script

--- .jenkins/get-artifact.sh
+++ .jenkins/get-artifact.sh
@@ -2,8 +2,8 @@
 
 echo "Collecting artifact"
 cp -r ./.next/standalone/ ./build
-cp -r ./public ./build
-cp -r ./static ./build
+cp -r ./public ./build/.next
+cp -r ./.next/standalone ./build/.next/
 
 echo "Zipping artifact"
 zip -r build.zip build
