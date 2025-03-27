971d5af240a4bed8e57532a31759c1b6f97be73e
OleksiuDatsko <datskooleksiu@gmail.com>
2025-03-27 12:50:44 +0200

    Update helm charts

--- .helm/minikube-start.sh
+++ .helm/minikube-start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 minikube delete
+sleep 5
 minikube start --addons=ingress --ports=80:80 --memory=no-limit --cpus=no-limit
 
-helm secrets install -f values.yaml -f secrets.values.yaml jira-clone-01 . --set runSeed=true --set runMigrations=true
-helm install datadog-agent datadog/datadog -f values.yaml
+sleep 20
+helm secrets install -f jira-clone/values.yaml -f jira-clone/secrets.values.yaml jira-clone-01 jira-clone --set runSeed=true --set runMigrations=true
+helm install datadog-agent datadog/datadog -f jira-clone/values.yaml
